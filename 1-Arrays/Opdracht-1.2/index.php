23e166c310397bfd7ca5806d2e7cece1c906fc76
noah elmont <2047541@talnet.nl>
2022-03-01 12:50:56 +0100

    opdracht 1.2

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,11 @@
+<?php
+$persoon = array(
+    "voornaam"=>"Noah",
+    "achternaam"=>"Elmont",
+    "leeftijd"=>"21 jaar",
+    "opleiding"=>"software developer"
+);
+
+echo $persoon["voornaam"]. $persoon["achternaam"]."is". $persoon["leeftijd"]."en is student aan het ROC van Amsterdam aan de opleiding". $persoon["opleiding"];
+ 
+?>

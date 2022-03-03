f90e674e9941cb1c1b8f433dc6bb856c376cef3a
noah elmont <2047541@talnet.nl>
2022-03-03 13:09:56 +0100

    php

---
+++ 2-Loops/Opdracht-2.2/loops-for.php
@@ -0,0 +1,13 @@
+<?php 
+
+    for($beginwaarde = 0; $beginwaarde <= 18; $beginwaarde++){
+        
+        if($beginwaarde =+ 18){
+            echo "<ul> ik ben $beginwaarde++ oud dus ik mag stemmen! </ul>";
+        }
+        else{
+            echo " <ul> ik ben $beginwaarde jaar, dus ik mag nog niet stemmen </ul>";
+        }
+        
+    }
+?>
